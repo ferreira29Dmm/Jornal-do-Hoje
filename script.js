9acd6b56b847cd64590309f3e731e65ea0339834
ferreira29Dmm <ferreira@dmmconsultoria.com.br>
2025-07-17 22:45:20 -0300

    adicionando scroll opacity

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
 var controller = new ScrollMagic.Controller();
 
-// Seleciona todas as seções com a classe .manchete__container
-document.querySelectorAll('#manchete__container').forEach((el) => {
+document.querySelectorAll('.scroll__opacity').forEach(function(section){
   new ScrollMagic.Scene({
-    triggerElement: el,
-    triggerHook: 0.8,
-    reverse: false
-  })
-    .setClassToggle(el, 'fade-in')
-    .addTo(controller);
-});
+  triggerElement: section,
+  triggerHook: 0.3,
+})
+  .setClassToggle(section, 'fade-in')
+  .addIndicators()
+  .addTo(controller)
+})
+
