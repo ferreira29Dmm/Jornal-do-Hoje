5705f5d0f91740735c827bb5701590b1d338ffe9
ferreira29Dmm <ferreira@dmmconsultoria.com.br>
2025-07-23 14:04:19 -0300

    initial scroll opacity

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 var controller = new ScrollMagic.Controller();
+var controller2 = new ScrollMagic.Controller();
 
 document.querySelectorAll('.scroll__opacity').forEach(function(section){
   new ScrollMagic.Scene({
@@ -7,5 +8,14 @@ document.querySelectorAll('.scroll__opacity').forEach(function(section){
 })
   .setClassToggle(section, 'fade-in')
   .addTo(controller)
+})
+
+document.querySelectorAll('.scroll__opacity__initial').forEach(function(section){
+  new ScrollMagic.Scene({
+  triggerElement: section,
+  triggerHook: 0.35,
+})
+  .setClassToggle(section, 'fade-in')
+  .addTo(controller2)
 })
 
