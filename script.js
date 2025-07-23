56b1befc1f363bc910b2893061edb9f07f062876
ferreira29Dmm <ferreira@dmmconsultoria.com.br>
2025-07-23 10:54:23 -0300

    Update script.js

--- script.js
+++ script.js
@@ -3,7 +3,7 @@ var controller = new ScrollMagic.Controller();
 document.querySelectorAll('.scroll__opacity').forEach(function(section){
   new ScrollMagic.Scene({
   triggerElement: section,
-  triggerHook: 0.4,
+  triggerHook: 0.7,
 })
   .setClassToggle(section, 'fade-in')
   .addTo(controller)
